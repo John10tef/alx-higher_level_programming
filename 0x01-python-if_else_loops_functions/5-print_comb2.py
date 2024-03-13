5f7fdd0d16ae47318d02d08d1d00ab6586f6f56e
John10tef <yohannes.teferi10@gmail.com>
2024-03-13 18:37:47 +0000

    task 5 done 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(("0" if i < 10 else "") + str(i), end=", " if i < 99 else "\n")
+    print(("{:0}" if i < 10 else "{}").format(i), end=", " if i < 99 else "\n")
