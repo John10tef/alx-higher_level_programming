53d4f299e3056075183e0ec79d9ccf6ac7cb978e
John10tef <yohannes.teferi10@gmail.com>
2024-04-22 06:55:31 -0700

    late

--- 0x02-python-import_modules/1-calculator.py
+++ 0x02-python-import_modules/1-calculator.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import mul
-from calculator_1 import div
+from calculator_1 import add, sub, mul, div
 
-a = 10 
+a = 10
 b = 5
 
-ad = add(a, b)
-su = sub(a, b)
-mu = mul(a, b)
-di = div(a, b)
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
 
- 
-print("{} + {} = {}".format(a, b, ad))
-print("{} - {} = {}".format(a, b, su))
-print("{} * {} = {}".format(a, b, mu))
-print("{} / {} = {}".format(a, b, di))
-
+print("{0:d} + {1:d} = {2:d}".format(a, b, result_add))
+print("{0:d} - {1:d} = {2:d}".format(a, b, result_sub))
+print("{0:d} * {1:d} = {2:d}".format(a, b, result_mul))
+print("{0:d} / {1:d} = {2:d}".format(a, b, result_div))
